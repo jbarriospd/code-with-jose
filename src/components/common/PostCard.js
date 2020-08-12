90dccccbc36715f45803e03ff858e7c70175a8d4
Jose Barrios <jbarriospd@gmail.com>
2020-08-12 18:45:36 -0500

    [Update] Dependences

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -13,7 +13,7 @@ const PostCard = ({ post }) => {
             <header className="post-card-header">
                 {post.feature_image &&
                     <div className="post-card-image" style={{
-                        backgroundImage: `url(${post.feature_image})` ,
+                        backgroundImage: `url(${post.feature_image})`,
                     }}></div>}
                 {post.tags && <div className="post-card-tags"> <Tags post={post} visibility="public" autolink={false} /></div>}
                 {post.featured && <span>Featured</span>}
@@ -24,14 +24,14 @@ const PostCard = ({ post }) => {
                 <div className="post-card-footer-left">
                     <div className="post-card-avatar">
                         {post.primary_author.profile_image ?
-                            <img className="author-profile-image" src={post.primary_author.profile_image} alt={post.primary_author.name}/> :
-                            <img className="default-avatar" src="/images/icons/avatar.svg" alt={post.primary_author.name}/>
+                            <img className="author-profile-image" src={post.primary_author.profile_image} alt={post.primary_author.name} /> :
+                            <img className="default-avatar" src="/images/icons/avatar.svg" alt={post.primary_author.name} />
                         }
                     </div>
-                    <span>{ post.primary_author.name }</span>
+                    <span>{post.primary_author.name}</span>
                 </div>
                 <div className="post-card-footer-right">
-                    <div>{readingTime}test</div>
+                    <div>{readingTime}</div>
                 </div>
             </footer>
         </Link>
